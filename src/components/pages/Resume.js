20d094a799bb9cdcd6fa10a0fb4b6b9fb240c65e
nshaw973 <nshaw973@gmail.com>
2023-05-21 19:04:09 -0700

    added more to the resume section.

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -4,7 +4,7 @@ import jsPDF from 'jspdf';
 export default function Resume() {
   const generatePDF = () => {
     const doc = new jsPDF();
-    doc.text(resumeText, 10, 10); // Example content, modify as needed
+    doc.text(resumeText, 10, 10);
 
     doc.save(`Nathan Shaw's Resume.pdf`);
   };
@@ -29,8 +29,33 @@ export default function Resume() {
 
   return (
     <div>
-      <button onClick={generatePDF} className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Download Resume</button>
-      <p>{resumeText}</p>
+      <button
+        onClick={generatePDF}
+        className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 m-7"
+      >
+        Download Resume
+      </button>
+      <div className='ml-24 font-semibold'>
+        <h3>Front-End Proficencies:</h3>
+        <ul>
+          <li>React</li>
+          <li>Bootstrap</li>
+          <li>Tailwind</li>
+          <li>Javascript</li>
+          <li>CSS</li>
+          <li>Web APIs</li>
+        </ul>
+        <h3>Back-End Proficencies:</h3>
+        <ul>
+          <li>MySQL</li>
+          <li>Sequelize</li>
+          <li>NoSQL</li>
+          <li>Mongoose</li>
+          <li>Insomnia</li>
+          <li>NodeJS</li>
+          <li>ExpressJS</li>
+        </ul>
+      </div>
     </div>
   );
 }
